b0d609a3ca480677767c5849faa74c4ffa45639f
Your Name <you@example.com>
2022-06-17 22:45:06 -0400

    explored setState secondary callBack function

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,38 +1,52 @@
-import {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      name:{firstName: 'Someone', lastName:'New'},
-      work: 'Bank'
+      name: { firstName: "Someone", lastName: "New" },
+      work: "Bank",
     };
   }
 
-  render(){
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Howdy {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.work}
-        </p>
-        <button onClick={() => this.setState({name:{firstName:'You', lastName:'Tube'}})}>
-          Change Name
-        </button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Howdy {this.state.name.firstName} {this.state.name.lastName}, I work
+            at {this.state.work}
+          </p>
+          <button
+            onClick={() => {
+              this.setState(
+                () => {
+                  return {
+                    name: { firstName: "You", lastName: "Tube" },
+                  };
+                },
+                () => {
+                  console.log(this.state);
+                }
+              );
+            }}
+          >
+            Change Name
+          </button>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 export default App;

ee4440bc57b90a3c786b0e660c427a68ef99ce93
Your Name <you@example.com>
2022-06-18 22:29:33 -0400

    used map to print UI elements

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -6,45 +6,24 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: { firstName: "Someone", lastName: "New" },
-      work: "Bank",
+      monsters: [
+      {name: 'Who'},
+      {name: 'What'},
+      {name: 'Where'},
+      {name: 'Why'}]
     };
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Howdy {this.state.name.firstName} {this.state.name.lastName}, I work
-            at {this.state.work}
-          </p>
-          <button
-            onClick={() => {
-              this.setState(
-                () => {
-                  return {
-                    name: { firstName: "You", lastName: "Tube" },
-                  };
-                },
-                () => {
-                  console.log(this.state);
-                }
-              );
-            }}
-          >
-            Change Name
-          </button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.monsters.map(
+          (monster) => {
+            return <h1> {monster.name}</h1>;
+        }
+        )}
+
+
       </div>
     );
   }

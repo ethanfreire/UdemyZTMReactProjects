6546265c82b3e08a766ecdc118032c68c5601fc4
Your Name <you@example.com>
2022-06-18 22:47:45 -0400

    explored why key prop is impt in React

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -7,23 +7,24 @@ class App extends Component {
     super();
     this.state = {
       monsters: [
-      {name: 'Who'},
-      {name: 'What'},
-      {name: 'Where'},
-      {name: 'Why'}]
+        { name: "Who", id: 0 },
+        { name: "What", id: 1 },
+        { name: "Where", id: 2 },
+        { name: "Why", id: 3 },
+      ],
     };
   }
 
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map(
-          (monster) => {
-            return <h1> {monster.name}</h1>;
-        }
-        )}
-
-
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1> {monster.name}</h1>
+            </div>
+          );
+        })}
       </div>
     );
   }

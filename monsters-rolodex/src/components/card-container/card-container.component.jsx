d2b7313173782bf127f5e0a987f42808c078b055
ethanfreire <element55star@gmail.com>
2022-07-27 11:56:10 -0400

    changed remaining class component into functional component

--- monsters-rolodex/src/components/card-container/card-container.component.jsx
+++ monsters-rolodex/src/components/card-container/card-container.component.jsx
@@ -1,20 +1,16 @@
-import { Component } from "react";
 import "./card-container.styles.css";
-class CardContainer extends Component {
-  render() {
-    const { monster } = this.props;
+const CardContainer = ({ monster }) => {
 
-    const { name, email, id } = monster;
-    return (
-      <div className="card-container" key={id}>
-        <img
-          alt={`monster ${name}`}
-          src={`https://robohash.org/${id}?set=set2$size=180x180`}
-        ></img>
-        <h2>{name}</h2>
-        <p>{email}</p>
-      </div>
-    );
-  }
-}
+  const { name, email, id } = monster;
+  return (
+    <div className="card-container" key={id}>
+      <img
+        alt={`monster ${name}`}
+        src={`https://robohash.org/${id}?set=set2$size=180x180`}
+      ></img>
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  );
+};
 export default CardContainer;
--- monsters-rolodex/src/components/card-list/card-list.component.jsx
+++ monsters-rolodex/src/components/card-list/card-list.component.jsx
@@ -1,19 +1,12 @@
-import { Component } from "react";
 import "./card-list.styles.css";
 import CardContainer from "../card-container/card-container.component.jsx";
-class CardList extends Component {
-  render() {
-    // console.log(this.props.monsters);
-    // console.log("render from cardlist");
-    const { monsters } = this.props;
-    const{id} = monsters;
-    return (
-      <div className="card-list" key={id}>
-        {monsters.map((monster) => {
-          return <CardContainer monster={monster}></CardContainer>;
-        })}
-      </div>
-    );
-  }
-}
+const CardList = ({ monsters }) => {
+  return (
+    <div className="card-list" >
+      {monsters.map((monster) => {
+        return <CardContainer monster={monster}></CardContainer>;
+      })}
+    </div>
+  );
+};
 export default CardList;
